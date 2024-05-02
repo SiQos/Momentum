7de5a9a1bcbb22a138f8754832bda56030fe4f58
SiQo <95127917+SiQos@users.noreply.github.com>
2024-05-02 18:20:16 +0500

    refactor: add correct links to audio

--- js/playList.js
+++ js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
   {
     title: 'Code 46',
-    src: '../assets/audio/Code 46.mp3',
+    src: './assets/audio/Code 46.mp3',
     duration: '03:29'
   },
   {
     title: 'Faded',
-    src: '../assets/audio/Faded.mp3',
+    src: './assets/audio/Faded.mp3',
     duration: '02:58'
   },
   {
     title: 'Play',
-    src: '../assets/audio/Play.mp3',
+    src: './assets/audio/Play.mp3',
     duration: '02:48'
   },
   {
     title: 'See Me Fall',
-    src: '../assets/audio/See Me Fall.mp3',
+    src: './assets/audio/See Me Fall.mp3',
     duration: '03:34'
   },
 ]
